4e01d943cacdf45dbc3fd41db5cd42ebc730d1e7
Vera <escolar.geovanni@gmail.com>
2023-03-13 10:17:20 -0600

    agregamos comentarios

--- main.c
+++ main.c
@@ -12,17 +12,21 @@ void destruirVector(double*);
 //funcion principal main
 int main(void)
 {
+    //Definimos la variable apuntadora
     double  *X;
     int N;
     puts("Escriba el numero de elementos del vector ... \n ");
+    //Mandamos a llamar la funcion validar arreglo
     N=validarDimension();
     printf("Construyendo el arreglo dinamico ... \n");
+    //Cracion del vector 
     X = construirVector(N);
     printf("Escriba los valores de estos elementos ... \n ");
     leerVector(N,X);
     printf("Los valores almacenados en el vector son ... \n");
     imprimirVector(N,X);
     printf("Destruyendo el arreglo dinamico...\n");
+    //Destruyendo el vector
     destruirVector(X);
     puts("Fin del programa ...");
     return 0;
