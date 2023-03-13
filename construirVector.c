6245238768d5f6e2e47ec56aed67ab8565ef6a20
Geovanni-Vera <126904908+Geovanni-Vera@users.noreply.github.com>
2023-03-13 12:38:18 -0600

    Update construirVector.c

--- construirVector.c
+++ construirVector.c
@@ -1,14 +1,18 @@
 /*ConstruirVector.c*/
 #include "librerias.h"
 
+//se crea el espacio de memoria para el vector
 double *construirVector(int n)
 {
     double *arr;
     arr = (double*)calloc(n,sizeof(double));
+    
+    //Si no hya espacio de memoria termina la ejecucion del program
     if(arr == NULL)
     {
         printf("Error, no hay memoria disponible !! \n");
         exit(1);
     }
+    //Regresa el verctor arr
     return arr;
 }
