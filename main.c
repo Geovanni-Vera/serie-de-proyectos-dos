f20678e5f050c689c921ea60fc9051691740be97
Geovanni-Vera <escolar.geovanni@gmail.com>
2023-03-24 08:12:23 -0600

    Bug arreglado ';'

--- main.c
+++ main.c
@@ -5,47 +5,22 @@
 
 //prototipos para poder usar funciones
 int validarDimension(void);
-double *construirVector(int);
-void leerVector(int,double*);
-void imprimirVector(int,double*);
-double calcularMedia(int, double*);
-double calcularVarianza(int,double*,double);
-double desviacionEstandar(double);
-void destruirVector(double*);
+void leerMatriz(char,int,int,double[][NMC]);
+void imprimirMatriz(int,int,double[][NMC]);
 
 //funcion principal main
 int main(void)
 {
-
-    double M,V,DS;
-    //Definimos la variable apuntadora
-    double  *X;
-    int N;
-    puts("Escriba el numero de elementos del vector ... \n ");
-    //Mandamos a llamar la funcion validar arreglo
-    N=validarDimension();
-    printf("Construyendo el arreglo dinamico ... \n");
-    //Cracion del vector 
-    X = construirVector(N);
-    printf("Recuperando los datos del archivo ... \n ");
-    leerVector(N,X);
-    printf("Los valores almacenados en el vector son ... \n");
-    imprimirVector(N,X);
-    //Realizacion de la media
-    printf("Realizando el calculo de la media...\n");
-    M=calcularMedia(N,X);
-    printf("La media aritmetica es: %1f\n",M);
-    //Realizacion de la Varianza
-    puts("Realizando el calculo de la varianza ... \n");
-    V=calcularVarianza(N,X,M);
-    printf("La varianza es : %1f .... \n",V);
-    //Realizacion de la desviacion estandar
-    puts("Calculando la desviacion estandar ...");
-    DS = desviacionEstandar(V);
-    printf("La desviacion estandar es: %1f \n",DS);
-    printf("Destruyendo el arreglo dinamico...\n");
-    //Destruyendo el vector
-    destruirVector(X);
+    double  A[NMR][NMC];
+    int N,M;
+    printf("Escribe el numer de renglones de la matriz ... \n");
+    M = validarDimension();
+    printf("Escribe el numero de columnas de la matriz ...\n");
+    N = validarDimension();
+    printf("Introduzca los elementos de la matriz ... \n ");
+    leerMatriz('A',M,N,A);
+    printf("Los valores almacenados en la matriz son ... \n");
+    imprimirMatriz(M,N,A);
     puts("Fin del programa ...");
     return 0;
 }
